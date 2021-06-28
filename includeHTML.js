889c9653be2defda19f41a9170824f9650c1f081
phil <46215388+PhilGitH@users.noreply.github.com>
2021-06-28 17:52:18 +0700

    Tracker Publish

---
+++ includeHTML.js
@@ -0,0 +1,38 @@
+/* HTML inclusion script. */
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            /* Remove the attribute, and call this function once more: */
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        return;
+      }
+    }
+
+	if (localStorage["thispage"] !== undefined && localStorage["thispage"] !== null) {
+		document.querySelector("div").innerHTML = localStorage["thispage"];
+	}
+
+	$(document).on('click', "div > a", function() {
+		localStorage.removeItem("thispage");
+		this.classList.toggle("highlight");
+		localStorage["thispage"] = document.querySelector("div").innerHTML;
+	});
+}
